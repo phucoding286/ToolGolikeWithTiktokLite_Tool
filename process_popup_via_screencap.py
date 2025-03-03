8c5b0fa4cc7ae6c4f9a40135a0c851bc5dc1bbb3
Programing Kun <phucoding286@gmail.com>
2025-03-03 19:23:38 +0700

    Add files via upload

--- process_popup_via_screencap.py
+++ process_popup_via_screencap.py
@@ -1,25 +1,39 @@
 from modules import *
 
+vocab = {
+    "a": list("áàảãạăắằẳẵặâấầẩẫậ"),
+    "e": list("éèẻẽẹêếềểễệ"),
+    "i": list("íìỉĩị"),
+    "o": list("óòỏõọôốồổỗộơớờởỡợ"),
+    "u": list("úùủũụưứừửữự"),
+    "y": list("ýỳỷỹỵ")
+}
+
 def detect_popup(img):
     str_detected = pytesseract.image_to_string(img).splitlines()
+    for i in range(len(str_detected)):
+        for key, value in vocab.items():
+            for char in value:
+                str_detected[i] = str_detected[i].replace(char, key)
+                
     # print(str_detected)
     
-    if "Dang xuat?" in str_detected:
+    if "Dang xuat?" in str_detected or "Dang xuat" in str_detected:
         return "Đăng xuất?"
     
-    elif 'Luu thong tin dang nhap?' in str_detected:
+    elif 'Luu thong tin dang nhap?' in str_detected or 'Luu thong tin dang nhap' in str_detected:
         return "Lưu thông tin đăng nhập?"
     
     elif "Follow ban be cua ban" in str_detected:
         return "Follow bạn bè của bạn"
     
-    elif "Thém ban bé, dung TikTok t" in str_detected:
+    elif "Them ban be, dung TikTok t" in str_detected:
         return "Thêm bạn bè, dùng Tiktok t"
     
-    elif "Déng bé danh sach ban be" in str_detected:
+    elif "Deng be danh sach ban be" in str_detected:
         return "Đồng bộ danh sách bạn bè"
 
-    elif "Thém ban bé, dung TikTok" in str_detected:
+    elif "Them ban be, dung TikTok" in str_detected:
         return "Thêm bạn bè, dùng TikTok"
     
     elif "Trang thai tai knoan" in str_detected:
@@ -34,5 +48,5 @@ def screencap(adb_path, device_id):
     os.system(f'{adb_path} -s {device_id} pull /storage/emulated/0/Download/screenshot.png ./screenshot.png')
 
 if __name__ == "__main__":
-    screencap(open("adb_path.txt").read(), "351a9fc")
+    screencap(open("adb_path.txt").read(), "192.168.1.56:5555")
     print(popup_processing())
