447412aa8b7a1d34da078be8fbe7a9f943619024
Programing Kun <phucoding286@gmail.com>
2025-04-29 01:25:48 +0700

    Add files via upload

--- check_dead_accounts.py
+++ check_dead_accounts.py
@@ -17,11 +17,24 @@ print(wait_color(f"[..] Đợi để chạy tool check dead/live account tiktok"
 
 for account in check_response:
     aid, auname = account[0], account[1]
-    response = scraper.get(f"https://www.tiktok.com/@{auname}")
-    if len(response.text.split("followingCount\":")) <= 1:
+    
+    # try 2 times
+    is_dead: bool = False
+    for _ in range(2):
+        response = scraper.get(f"https://www.tiktok.com/@{auname}")
+        if len(response.text.split("followingCount\":")) <= 1:
+            time.sleep(2)
+            is_dead = True
+            continue
+        else:
+            is_dead = False
+            break
+
+    if is_dead:
         print(error_color(f"[!] Username tiktok: {auname}, Đã dead!"))
     else:
         print(success_color(f"[*] Username tiktok: {auname}, Vẫn còn hoạt động."))
+
     time.sleep(2)
 
 print()
