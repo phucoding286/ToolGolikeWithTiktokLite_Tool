909e893b34b1e3c642cbc6f3c107380d0e51043e
Programing Kun <phucoding286@gmail.com>
2025-06-03 18:37:28 +0700

    Add files via upload

--- upload_image.py
+++ upload_image.py
@@ -18,7 +18,7 @@ def upload_image(driver, adb_path, device_id):
 
         img_choose = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located(
-                    (By.XPATH, "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.LinearLayout/android.view.ViewGroup/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.widget.GridView/android.widget.FrameLayout[1]/android.widget.ImageView")
+                    (By.XPATH, "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.LinearLayout/android.view.ViewGroup/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.widget.GridView/android.widget.FrameLayout[2]/android.widget.ImageView")
                 )
             )
         img_choose.click()
@@ -38,6 +38,6 @@ def upload_image(driver, adb_path, device_id):
 
 if __name__ == "__main__":
     adb_path = open("adb_path.txt", "r").read()
-    driver = driver_init(adb_path, ask_udid=False, device_id="351a9fc", appium_port="1000")
-    print(upload_image(driver, adb_path, device_id="351a9fc"))
+    driver = driver_init(adb_path, ask_udid=False, device_id="192.168.1.56:5555", appium_port="1000")
+    print(upload_image(driver, adb_path, device_id="192.168.1.56:5555"))
     input(">>> ")
